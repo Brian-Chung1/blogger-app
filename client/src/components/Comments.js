7dc48d86d4e445e33e00d0c840196bc029d83825
Brian-Chung1 <brian.chung.cs@gmail.com>
2021-04-14 15:59:32 -0700

    made comment avatars clickable and link to user

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -5,15 +5,38 @@ import CardContent from '@material-ui/core/CardContent';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 import { dateFormatter } from '../utils/index';
+import { useHistory } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+  avatarHover: {
+    cursor: 'pointer',
+  },
+}));
+
 const Comments = ({ commentObj }) => {
+  let history = useHistory();
+  const classes = useStyles();
   const { comment, timestamp, username } = commentObj;
+
+  const viewUser = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    history.push(`/user/${username}`);
+  };
 
   return (
     <Card>
       <CardHeader
-        avatar={<Avatar aria-label="avatar">{username.charAt(0)}</Avatar>}
+        avatar={
+          <Avatar
+            onClick={viewUser}
+            aria-label="avatar"
+            className={classes.avatarHover}
+          >
+            {username.charAt(0)}
+          </Avatar>
+        }
         title={username}
         subheader={dateFormatter(timestamp)}
       />
